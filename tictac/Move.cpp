80b1a4437c01418e048e25ea42c7196db47eea0f
yuxuanchen <yuxuanchen@ucsb.edu>
2023-04-21 12:42:08 -0700

    t

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -8,22 +8,72 @@ using namespace std;
 
 Move::Move(const std::string& input) {
 	std::istringstream iss (input);
+	string temp;
+	if (!(iss >> temp)){
+		throw ParseError("no number");
+	}
+	else if (temp.size() != 1){
+		throw ParseError("wrong size for number");
+	}
+	else if ((temp < 1) || (temp > 9)) {
+		throw ParseError("number out of range");
+	}
+	else {
+		number = temp;
+	}
+
+	if (!(iss >> temp)){
+		throw ParseError("no player");
+	}
+	else if (temp.size() != 1) {
+		throw ParseError("wrong size for player");
+	}
+	else if ((toupper(temp) != "X") && (toupper(temp) != "O")){
+		throw ParseError("player not X or O");
+	}
+	else {
+		player = toupper(temp);
+	}
+
+	if (!(iss >> temp)){
+		throw ParseError("no row/column");
+	}
+	else if (temp.size() != 2) {
+		throw ParseError("wrong size for row/column");
+	}
+	else if (((temp[0]-"A")!= 0) && ((temp[0]-"A")!=1) && ((temp[0]-"A")!=2)){
+		throw ParseError("invalid row");
+	}
+	else if (((temp[1]-"0")!= 1) && ((temp[1]-"0")!= 2) && ((temp[1]-"0")!= 3)){
+		throw ParseError("invalid column");
+	}
+	else {
+		row = temp[0] -"A" +1;
+		column = temp[1] -"0";
+	}
+
+
+/*
 	iss >> number;
+	if ((number < 1) || (number > 9)){
+		throw ParseError("invalid number");
+	}
 	iss >> player;
 	player = toupper(player);
+	if ((player != "X") || (player != "O"))
 	string temp;
 	iss >> temp;
 	row = temp[0] - 'A';
 	column = temp[1]- '0' +1;
-
+*/
 }
 
 std::ostream& operator << (std::ostream& stream, const Move& move) {
 	stream << move.number << " " << move.player << " ";
-	if (move.row ==0) {
+	if (move.row ==1) {
 		stream << "A";
 	}
-	else if (move.row ==1) {
+	else if (move.row ==2) {
 		stream << "B";
 	}
 	else {
