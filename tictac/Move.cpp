8d00b28ca4817bc58418f137f73a345d276f3d7e
yuxuanchen <yuxuanchen@ucsb.edu>
2023-04-21 12:07:25 -0700

    t

--- tictac/Move.cpp
+++ tictac/Move.cpp
@@ -1,12 +1,33 @@
 #include "Errors.h"
 #include "Move.h"
+#include <sstream>
+#include <string>
 
+using namespace std
 // Space for implementing Move functions.
 
 Move::Move(const std::string& input) {
+	std::istringstream iss (input);
+	iss >> number;
+	iss >> player;
+	player = toupper(player);
+	string temp;
+	iss >> temp;
+	row = temp[0] - 'A';
+	column = temp[1]- '0' +1;
 
 }
 
 std::ostream& operator << (std::ostream& stream, const Move& move) {
-
+	stream << number << " " << player << " ";
+	if (row ==0) {
+		stream << "A";
+	}
+	else if (row ==1) {
+		stream << "B";
+	}
+	else {
+		stream << "C";
+	}
+	stream << column;
 }
