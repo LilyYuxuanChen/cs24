176c28d7a4d87b47ea5861727b92ac68918a7ae1
yuxuanchen <yuxuanchen@ucsb.edu>
2023-04-27 15:41:08 -0700

    setree

--- setree/Node.h
+++ setree/Node.h
@@ -1,7 +1,21 @@
-#ifndef NODE_H
+#ifndef NODE
 #define NODE_H
-
+using namespace std;
 // Use this file to declare your Node type.
 // Implement Node member functions and helper functions in Node.cpp.
+class Node{
+		string value;
+		Node* left;
+		Node* right;
+		size_t count;
+	public:
+		Node();
+		Node(string val);
+ 
+		~Node:();
+		Node (const Node& other);
+		int node() const;
+		Node(Node&& other); //constructor
 
+}
 #endif
