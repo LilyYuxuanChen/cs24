f66e40fce9f59cd843a36703ac6eb9d45e2bf310
yuxuanchen <yuxuanchen@ucsb.edu>
2023-04-28 11:30:37 -0700

    test

--- setree/Set.cpp
+++ setree/Set.cpp
@@ -1,1 +1,29 @@
 #include "Set.h"
+#include <cstddef>
+#include "Node.h"
+
+size_t Node::clear(){
+	return 0;
+}
+bool Node::contains(const std::string& value) const {
+	return false;
+}
+size_t Node::count() const {
+	return 0;
+}
+void Node::debug() {
+}
+size_t Node::insert(const std::string& value){
+	return 0;
+}
+const std::string& Node::lookup(size_t n) const {
+	return nullptr;
+}
+
+void Node::print() const{
+
+}
+size_t Node::remove(const std::string& value) {
+	return 0;
+}
+
