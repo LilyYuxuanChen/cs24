1675dcc7f4b9d7f1cff8bc1f89189dbfe9d3354a
yuxuanchen <yuxuanchen@ucsb.edu>
2023-05-15 17:28:44 -0700

    t

--- counter/Counter.cpp
+++ counter/Counter.cpp
@@ -48,8 +48,8 @@ Node* Counter::find(const std::string& key) const{
 
 Counter::Counter() {
 	cnt = 0;
-	cap = 2147483647;
-	table = new Node*[2147483647]();
+	cap = 21474836;
+	table = new Node*[21474836]();
 	ibegin = new Iterator();
 	ibegin->i = nullptr;
 	iend = new Iterator();
