237a628e8d7ee78cfb7c41be654e4ddf1300a59f
yuxuanchen <yuxuanchen@ucsb.edu>
2023-05-21 22:08:12 -0700

    t

---
+++ typo/Heap.cpp
@@ -0,0 +1,143 @@
+#include "Heap.h"
+#include <iostream>
+#include <stdexcept>
+
+Heap::Heap(size_t capacity) {
+	mCapacity = capacity;
+	mCount = 0;
+	mData = new Entry[capacity];
+
+}
+Heap::Heap(const Heap& other) {
+	mCapacity = other.mCapacity;
+	mCount = other.mCount;
+	mData = new Entry[mCapacity];
+	for (size_t i = 0; i < mCapacity; i++) {
+		mData[i] = other.mData[i];
+	}
+
+}
+Heap::Heap(Heap&& other) {
+	mCapacity = other.mCapacity;
+	mCount = other.mCount;
+	mData = other.mData;
+	other.mCapacity = 0;
+	other.mCount = 0;
+	other.mData = NULL;	
+
+}
+
+Heap::~Heap() {
+	delete[] mData;
+	mData = NULL;
+}
+
+size_t Heap::capacity() const {
+	return mCapacity;
+}
+
+size_t Heap::count() const {
+	return mCount;
+}
+
+const Heap::Entry& Heap::lookup(size_t index) const {
+	if (index >= mCount) {
+		throw std::underflow_error("out of range");
+	}
+	else {
+		return mData[index];
+	}
+}
+
+Heap::Entry Heap::pop() {
+	if (mCount == 0) {
+		throw std::underflow_error("empty");
+	}
+	else {
+		Entry r = mData[0];
+		mData[0] = mData[mCount-1];
+		mCount -= 1;
+		size_t loc = 0;
+		size_t left = 1;
+		size_t right = 2;
+		if (left < mCount && right < mCount) {
+		while (mData[loc].score > mData[left].score || mData[loc].score > mData[right].score) {
+			if (mData[loc].score > mData[left].score && mData[loc].score <= mData[right].score) {
+				Entry temp;
+				temp = mData[loc];
+				mData[loc] = mData[left];
+				mData[left] = temp;
+				loc = left;
+			}
+			else if (mData[loc].score < mData[left].score && mData[loc].score >= mData[right].score){
+				Entry temp;
+				temp = mData[loc];
+				mData[loc] = mData[right];
+				mData[right] = temp;
+				loc = right;
+			}
+			else {
+				if (mData[left].score < mData[right].score) {
+					Entry temp;
+					temp = mData[loc];
+					mData[loc] = mData[left];
+					mData[left] = temp;
+					loc = left;
+				}
+				else if (mData[left].score = mData[right].score) {
+					Entry temp;
+					temp = mData[loc];
+					mData[loc] = mData[left];
+					mData[left] = temp;
+					loc = left;
+				}
+				else {
+					Entry temp;
+					temp = mData[loc];
+					mData[loc] = mData[right];
+					mData[right] = temp;
+					loc = right;
+				}
+			}
+			left = loc*2+1;
+			right = loc*2+2;
+			if (left >= mCount) {
+				break;
+			}
+			else if (right >= mCount && left < mCount) {
+				if (mData[loc].score < mData[left].score) {
+					Entry temp;
+					temp = mData[loc];
+					mData[loc] = mData[left];
+					mData[left] = temp;
+					loc = left;
+				}
+				break;
+			}
+	}
+	}
+	else if (right >= mCount && left < mCount) {
+	if (mData[loc].score < mData[left].score) {
+	                                        Entry temp;
+					                                        temp = mData[loc];
+									                                        mData[loc] = mData[left];
+													                                        mData[left] = temp;
+																	                                        loc = left;
+																					                                }	
+	}
+return r;
+}
+}
+
+Heap::Entry Heap::pushpop(const std::string& value, float score) {
+	Entry temp;
+	return temp;
+}
+
+void Heap::push(const std::string& value, float score) {
+
+}
+
+const Heap::Entry& Heap::top() const {
+	return mData[0];
+}
---
+++ typo/WordList.cpp
@@ -0,0 +1,13 @@
+#include "WordList.h"
+#include <memory>
+#include <cmath>
+using namespace std;
+
+WordList::WordList(std::istream& stream) {
+
+}
+Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
+	Heap a = new Heap(maxcount);
+	return a;
+}
+
