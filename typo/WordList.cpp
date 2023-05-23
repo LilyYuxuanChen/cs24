c47f0f83e13250bd5f85336f56445d124d0c6e1c
yuxuanchen <yuxuanchen@ucsb.edu>
2023-05-23 15:15:32 -0700

    t

--- typo/WordList.cpp
+++ typo/WordList.cpp
@@ -1,9 +1,23 @@
 #include "WordList.h"
 #include <memory>
 #include <cmath>
+#include <string>
 using namespace std;
 
 WordList::WordList(std::istream& stream) {
+	string wd;
+	while (stream >> wd) {
+		bool L = true;
+		for (char c : wd) {
+			if (!(islower(c))) {
+				L = false;
+				break;
+			}
+		}
+		if (L) {
+			mWords.push_back(wd);
+		}
+	}
 
 }
 Heap WordList::correct(const std::vector<Point>& points, size_t maxcount, float cutoff) const {
